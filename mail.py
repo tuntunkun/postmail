cd661ad1c789780ea470097fa36c69249d229bc3
Takuya Sawada <takuya@tuntunkun.com>
2014-01-20 00:19:08 +0900

    refactoring

--- mail.py
+++ mail.py
@@ -12,9 +12,15 @@ from email.Utils import COMMASPACE
 from email.Utils import formatdate
 from email import Encoders
 
-class App:
-	_sopts = []
-	_lopts = []
+class App(object):
+	class Option(object):
+		def __init__(self, sopt, lopt, type, desc):
+			self.sopt = sopt
+			self.lopt = lopt
+			self.type = type
+			self.desc = desc
+
+	__options = []
 
 	def __init__(self, argv):
 		self._init(argv)
@@ -22,16 +28,25 @@ class App:
 	def _pre_init(self):
 		pass
 
-	def _post_init(self):
+	def _post_init(self, args):
 		pass
 
 	def _parse(self, opt, arg):
-		pass
+		return False
+
+	def _short_opts(self):
+		return map(lambda x: x.sopt, filter(lambda x: not x.sopt is None, self.__options))
+
+	def _long_opts(self):
+		return map(lambda x: x.lopt, filter(lambda x: not x.lopt is None, self.__options))
+
+	def _append_option(self, sopt, lopt, type=None, desc=None):
+		self.__options.append(App.Option(sopt, lopt, type, desc))
 
 	def _init(self, argv):
 		self._pre_init()
 
-		opts, args = getopt.getopt(argv, ''.join(self._sopts), self._lopts)
+		opts, args = getopt.getopt(argv, ''.join(self._short_opts()), self._long_opts())
 		for opt, arg in opts:
 			self._parse(opt, arg)
 
@@ -58,67 +73,79 @@ class SMTPApp(App):
 	_msg_body = sys.stdin
 
 	def _pre_init(self):
-		self._lopts.append('tls')
+		super(SMTPApp, self)._pre_init()
+
+		self._append_option(None, 'tls')
 
 		# smtp
-		self._lopts.append('host=')
-		self._lopts.append('port=')
-		self._lopts.append('user=')
-		self._lopts.append('pass=')
+		self._append_option(None, 'host=', 'host')
+		self._append_option(None, 'port=', 'port')
+		self._append_option(None, 'user=', 'user')
+		self._append_option(None, 'pass=', 'password')
 
 		# message
-		self._lopts.append('from=')
-		self._lopts.append('to=')
-		self._lopts.append('cc=')
-		self._lopts.append('subject=')
-		self._lopts.append('body=')
-		self._lopts.append('attach=')
+		self._append_option(None, 'from=', 'email')
+		self._append_option(None, 'to=', 'email')
+		self._append_option(None, 'cc=', 'email')
+		self._append_option(None, 'subject=', 'text')
+		self._append_option(None, 'body=', 'file')
+		self._append_option(None, 'attach=', 'file')
 
 		# service
-		self._lopts.append('gmail')
-		self._lopts.append('o365')
+		self._append_option(None, 'gmail')
+		self._append_option(None, 'o365')
 
 	def _parse(self, opt, arg):
-		if opt == '--tls':
+		result = True
+
+		if super(SMTPApp, self)._parse(opt, arg):
+			return result
+
+		if opt in ('--tls',):
 			self._use_tls = True
 
-		elif opt == '--host':
+		elif opt in ('--host',):
 			self._smtp_host = arg
-		elif opt == '--port':
+		elif opt in ('--port',):
 			self._smtp_port = arg
-		elif opt == '--user':
+		elif opt in ('--user',):
 			self._smtp_auth = True
 			self._smtp_user = arg
-		elif opt == '--pass':
+		elif opt in ('--pass',):
 			self._smtp_pass = arg
 
-		elif opt == '--from':
+		elif opt in ('--from',):
 			self._msg_sender = arg
-		elif opt == '--to':
+		elif opt in ('--to',):
 			self._msg_recipients.append(arg)
-		elif opt == '--cc':
+		elif opt in ('--cc',):
 			self._msg_copies.append(arg)
-		elif opt == '--subject':
+		elif opt in ('--subject',):
 			self._msg_subject = arg
-		elif opt == '--body':
+		elif opt in ('--body',):
 			if not self._msg_body is sys.stdin:
 				self._msg_body.close()
 			self._msg_body = open(arg, 'rb')
-		elif opt == '--attach':
+		elif opt in ('--attach',):
 			self._msg_attachments.append(arg)
 
-		elif opt == '--gmail':
+		elif opt in ('--gmail',):
 			self._use_tls = True
 			self._smtp_auth = True
 			self._smtp_host = 'smtp.gmail.com'
 			self._smtp_port = 587
-		elif opt == '--o365':
+		elif opt in ('--o365',):
 			self._use_tls = True
 			self._smtp_auth = True
 			self._smtp_host = 'smtp.office365.com'
 			self._smtp_port = 587
+		else:
+			result = False
+		return result
 
-	def _post_init(self, agrs):
+	def _post_init(self, args):
+		super(SMTPApp, self)._post_init(args)
+
 		if len(self._msg_recipients) < 1:
 			raise Exception('you must specify at least one recipient email address')
 
